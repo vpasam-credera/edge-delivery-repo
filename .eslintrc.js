82f3e4bc0a48913d51a1cb3f631eb0b44ed85000
vpasam-credera <venkataramana.pasam@credera.com>
2024-12-14 21:08:49 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     requireConfigFile: false,
   },
   rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports 
+    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
   },
