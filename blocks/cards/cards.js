aae1800f2e275149a143d9711ef575b417637524
Venkataramana Pasam (Credera) <venkataramana.pasam@credera.com>
2024-12-16 11:24:38 +0530

    updated

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -9,12 +9,12 @@ export default function decorate(block) {
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else {
-        //Custom changes start
+        //  Custom changes start
         div.className = 'cards-card-body';
         const p = document.createElement('p');
         p.append(new Date().toDateString());
         div.prepend(p);
-      //Custom changes end
+      //  Custom changes end
       }
     });
     ul.append(li);
